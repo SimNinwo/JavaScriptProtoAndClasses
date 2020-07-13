354c39d1745bb4032f40515365d586380f15e48b
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-07-13 11:00:13 +0100

    using ES6 classes

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 'use strict';
 
 
-// function Cat(name, color) {
-//     this.name = name
-//     this.color = color
-// }
-
-// var cat = new Cat('Fluffy', 'White');
-
-var cat = Object.create(Object.prototype, {
-    name: {
-        value: 'Fluffy',
-        enumerable: true,
-        writable: true,
-        configurable: true
-    },
-    color: {
-        value: 'White',
-        enumerable: true,
-        writable: true,
-        configurable: true
+class Cat {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
     }
-})
+
+    speak() { display('Meeoow') }
+}
+
+var cat = new Cat('Fluffy', 'White');
+
 
 display(cat)
+cat.speak()
